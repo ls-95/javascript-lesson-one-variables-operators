0619197382307dafd19a7a9cc2970814c53edbe9
ls-95 <143622355+ls-95@users.noreply.github.com>
2026-03-16 12:09:34 +0100

    Add JS exercises code

--- src/lesson-one/lesson-one.js
+++ src/lesson-one/lesson-one.js
@@ -15,10 +15,58 @@ function handleConversion() {
 //Exercise 1:
 console.log("Hello, World!");
 
-//Exercise 2;
+//Exercise 2:
 let firstName = "Polly";
 let age = 57;
 let favouriteColor = "lavender";
 console.log(
   `Hi! My name is ${firstName} and I am ${age} years old. My favourite colour is ${favouriteColor}.`,
 );
+
+//Exercise 3
+let cats = 25;
+let dogs = 30;
+
+cats += 1;
+console.log(cats);
+dogs -= 5;
+console.log(dogs);
+
+let horse = 5;
+let rabbit = 10;
+
+let x;
+let y;
+x = horse * rabbit;
+console.log(x);
+y = horse / rabbit;
+console.log(y);
+
+x++;
+y++;
+console.log(x, y);
+
+x--;
+y--;
+console.log(x, y);
+console.log(typeof x);
+console.log(typeof y);
+
+cats -= 1;
+cats %= 2;
+console.log(cats % 2);
+
+let hasAccount = true;
+console.log(hasAccount);
+
+hasAccount = false;
+console.log(hasAccount);
+console.log(typeof hasAccount);
+
+hasAccount = null;
+console.log(typeof hasAccount);
+
+console.log(typeof (0 / 0));
+console.log(typeof NaN === "number");
+console.log(isNaN(0 / 0));
+console.log(NaN === NaN);
