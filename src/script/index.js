66ea87d002c1ce11a5ff6ebfdef5e7f03d7c7a14
ls-95 <143622355+ls-95@users.noreply.github.com>
2026-03-16 10:18:45 +0100

    Add basic styling. Add if statement if no value or not a number.

--- src/script/index.js
+++ src/script/index.js
@@ -1,8 +1,13 @@
 function handleConversion() {
   let input = document.getElementById("temperature").value;
   let number = Number(input);
-  let fahrenheit = number * 1.8 + 32;
-  console.log(fahrenheit);
   let result = document.getElementById("result");
+
+  if (isNaN(number) || input === "") {
+    alert("Please enter a valid number");
+    return;
+  }
+  let fahrenheit = Math.floor(number * 1.8 + 32);
+  console.log(fahrenheit);
   result.textContent = `${fahrenheit}°F`;
 }
