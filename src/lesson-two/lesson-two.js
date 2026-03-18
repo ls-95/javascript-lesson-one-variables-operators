2275a8022ca865092b635fb66155c88b5a19d8de
ls-95 <143622355+ls-95@users.noreply.github.com>
2026-03-18 09:52:44 +0100

    Undate html. Refactored JS code.

--- src/lesson-two/lesson-two.js
+++ src/lesson-two/lesson-two.js
@@ -2,15 +2,18 @@ let input = prompt("Please enter a price ($):", "$20.00");
 const pattern = /^\$\d+(\.\d{2})?$/;
 let result = document.getElementById("result");
 let inputPrice = document.getElementById("input");
+let discount = document.getElementById("discount");
 
 if (pattern.test(input)) {
-  let price = Number(input.replace("$", ""));
-  let discountedPrice = price - price / 10;
-  let roundedPrice = discountedPrice.toFixed(2);
-  console.log(discountedPrice);
-  console.log(roundedPrice);
+  let price = parseFloat(input.slice(1));
+  let discountPrice = price / 10;
+  let newPrice = price - discountPrice;
+  discount.textContent = `$${discountPrice.toFixed(2)}`;
   inputPrice.textContent = input;
-  result.textContent = `$${roundedPrice}`;
+  result.textContent = `$${newPrice.toFixed(2)}`;
+  console.log(`The price you entered is: $${input}`);
+  console.log(`Your new price is: $${newPrice.toFixed(2)}`);
+  console.log(`You saved: $${discountPrice.toFixed(2)}`);
 } else {
   alert("Invalid format. Please use something like $20.99");
 }
