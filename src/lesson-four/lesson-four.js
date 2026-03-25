1f6255c3112b3250868703c9c8a899541065d3e9
ls-95 <143622355+ls-95@users.noreply.github.com>
2026-03-25 11:36:14 +0100

    Add content and styling for roman numeral converter

--- src/lesson-four/lesson-four.js
+++ src/lesson-four/lesson-four.js
@@ -1,24 +1,24 @@
 //Exercise one
 
-function guessNumberGame() {
-  const target = Math.floor(Math.random() * 10) + 1;
-  let guess = null;
+// function guessNumberGame() {
+//   const target = Math.floor(Math.random() * 10) + 1;
+//   let guess = null;
 
-  while (guess !== target) {
-    guess = parseFloat(prompt("Enter a number"));
+//   while (guess !== target) {
+//     guess = parseFloat(prompt("Enter a number"));
 
-    console.log(`The target is: ${target}. Your guess is: ${guess}`);
+//     console.log(`The target is: ${target}. Your guess is: ${guess}`);
 
-    if (guess < target) {
-      console.log("You guess was too low");
-    } else if (guess > target) {
-      console.log("You guess was too high");
-    } else {
-      console.log(`Congratualtions, you guessed the correct number: ${guess}`);
-    }
-  }
-}
-guessNumberGame();
+//     if (guess < target) {
+//       console.log("You guess was too low");
+//     } else if (guess > target) {
+//       console.log("You guess was too high");
+//     } else {
+//       console.log(`Congratualtions, you guessed the correct number: ${guess}`);
+//     }
+//   }
+// }
+// guessNumberGame();
 
 //Exercise 2
 
